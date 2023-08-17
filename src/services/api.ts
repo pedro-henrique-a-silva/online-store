6194c144a441991302e9fe61a140239831eac55b
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-08-17 19:49:31 -0300

    finalizando requisito 1 e criando função extra

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,20 @@ export async function getCategories() {
   return (await fetch('https://api.mercadolibre.com/sites/MLB/categories')).json();
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId: string, query: string) {
+  return (await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`)).json();
+}
+
+export async function searchByIdCategoryOrQuery(search: string) {
+  const listCategories = await getCategories();
+  const findListCategories = listCategories.find((categorie:
+  { id: string, name:string, }) => categorie.id === search);
+  if (findListCategories) {
+    return (await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${search}`)).json();
+  }
+  if (findListCategories === undefined) {
+    return (await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=$${search}`)).json();
+  }
 }
 
 export async function getProductById() {
