c5333637a46d97cd1ed3f64e3b0b959088d95c6c
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-08-17 20:36:40 -0300

    corrigindo requisições

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,21 +2,24 @@ export async function getCategories() {
   return (await fetch('https://api.mercadolibre.com/sites/MLB/categories')).json();
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId: string, query: string) {
+export async function getProductsFromCategoryAndQuery(
+  categoryId = '',
+  query = '',
+) {
   return (await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)).json();
 }
 
-// export async function searchByIdCategoryOrQuery(search: string) {
-//   const listCategories = await getCategories();
-//   const findListCategories = listCategories.find((categorie:
-//   { id: string, name:string, }) => categorie.id === search);
-//   if (findListCategories) {
-//     return (await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${search}`)).json();
-//   }
-//   if (findListCategories === undefined) {
-//     return (await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=$${search}`)).json();
-//   }
-// }
+export async function searchByIdCategoryOrQuery(search: string) {
+  const listCategories = await getCategories();
+  const findListCategories = listCategories.find((categorie:
+  { id: string, name:string, }) => categorie.id === search);
+  if (findListCategories) {
+    return (await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${search}`)).json();
+  }
+  if (findListCategories === undefined) {
+    return (await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=$${search}`)).json();
+  }
+}
 
 export async function getProductById() {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
