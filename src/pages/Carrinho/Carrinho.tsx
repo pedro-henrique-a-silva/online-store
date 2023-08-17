b841ee28b45bbd984e9d8c0402438a023a6d8838
pacuzn <daviendizica@gmail.com>
2023-08-17 19:00:33 -0300

    req 3

---
+++ src/pages/Carrinho/Carrinho.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Carrinho() {
+  return (
+    <div>
+      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+    </div>
+  );
+}
+
+export default Carrinho;
