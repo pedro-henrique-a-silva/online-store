2ea4c7f1af1ea71c0b25992ebeffbba3dc563603
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-08-18 14:15:44 -0300

    Colocando conteudo do Search direto no home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import Search from '../../components/Search/Search';
+import Sidebar from '../../components/Sidebar/Sidebar';
 
 function home() {
   return (
-    <div>
-      <Search />
-    </div>
+    <>
+      <div>
+        <input type="text" />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+
+      <Sidebar />
+    </>
   );
 }
 
